55d11a13e4a11707efdd79b15e15c04cc1738996
dalbeertws <dalbeer.tws@tekkiwebsolutions.com>
2022-03-17 17:17:49 +0530

    update static

--- home/forms.py
+++ home/forms.py
@@ -1,61 +1,62 @@
-
+from django.contrib.auth.forms import UserCreationForm
+from . models import  User
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm,UsernameField
+from django.contrib.auth.forms import AuthenticationForm, UsernameField
 from django.utils.translation import gettext_lazy as _
 
-class LoginForm(AuthenticationForm):
-    username=forms.CharField(label='Username or Email',widget=forms.TextInput(
-       
-        attrs={'auto_focus':True,'class':'form-control',
-    
-    'placeholder':'Enter Username or Email',
-   
-    
 
-    }))
-    password=forms.CharField(
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(label='Username or Email', widget=forms.TextInput(
+
+        attrs={'auto_focus': True, 'class': 'form-control',
+
+               'placeholder': 'Enter Username or Email',
+
+
+
+               }))
+    password = forms.CharField(
         label='Password',
         strip=False,
         widget=forms.PasswordInput(attrs={
-            'autocomplete':'current_password',
-            'class':'form-control',
-            'placeholder':'Enter Password'
+            'autocomplete': 'current_password',
+            'class': 'form-control',
+            'placeholder': 'Enter Password'
         })
     )
-    # def set_field_html_name(cls, new_name):
-    #     """
-    #     This creates wrapper around the normal widget rendering, 
-    #     allowing for a custom field name (new_name).
-    #     """
-    #     old_render = cls.widget.render
-    #     def _widget_render_wrapper(name, value, attrs=None):
-    #         return old_render(new_name, value, attrs)
 
-    #     cls.widget.render = _widget_render_wrapper
-    # # After creating the field, call the wrapper with your new field name.
-    # set_field_html_name(username, 'email')
 
-#Creating Form for Registration
+# Creating Form for Registration
 # from django.contrib.auth.models import User
-from . models import Blog, User
 
-from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
-  email = forms.EmailField()
+    email = forms.EmailField()
 
-  class Meta:
-      model = User
-      fields = ['email', 'first_name','last_name']
-      widgets={
-          'email':forms.EmailInput(attrs={'class':'form-control'})
-      }
-# class AddPostForm(FormView):
-  
+    class Meta:
+        model = User
+        fields = ['email', 'first_name', 'last_name']
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'form-control'})
+        }
 
-#   class Meta:
-#       model = Blog
-#       fields = ['title', 'description','thumbnail']
-#       widgets={
-#           'email':forms.EmailInput(attrs={'class':'form-control'})
-#       }
+# class CommentForm(forms.Form):
+#     comment = forms.CharField(widget=forms.Textarea(attrs={
+#         'class': 'form-control',
+#         'placeholder': 'Enter Comment here',
+#         'cols': '5',
+#         'rows': '3'
+
+#     }))
+#     post_id = forms.CharField(widget=forms.HiddenInput())
+#     parent_Sno = forms.CharField(widget=forms.HiddenInput())
+
+
+# class ReplyCommentForm(forms.Form):
+#     reply = forms.CharField(widget=forms.Textarea(attrs={
+#         'class': 'form-control',
+#         'placeholder': 'Enter Comment here',
+#         'cols': '5',
+#         'rows': '3'
+
+#     }))
--- home/views.py
+++ home/views.py
@@ -1,28 +1,20 @@
 
-
-
-
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView,LogoutView
 from django.contrib.auth import login as auth_login
-from django.http import HttpResponseRedirect
 from . forms import LoginForm, UserRegisterForm
 from django.core.validators import validate_email
-from django.forms import ValidationError
 from .templatetags import extra_filter
 from .models import *
-import uuid
-from .helper import send_mail_registration
 from django.db.models import Q
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib import messages
 from django.shortcuts import redirect
-from django.views.generic import DetailView,ListView,CreateView,UpdateView,DeleteView
-from django.views.generic.edit import FormView,CreateView,ProcessFormView
+from django.views.generic import DetailView,ListView,CreateView,UpdateView,DeleteView,View
+from django.views.generic.edit import CreateView,ProcessFormView
 from django.utils.text import slugify
 from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
@@ -36,6 +28,71 @@ class Home(ListView):
 class ContinueReading(DetailView):
     model=Blog
     template_name='blog_post.html'
+    
+    # comment_form=CommentForm()
+    # reply_form=ReplyCommentForm()
+    def get_context_data(self, **kwargs):
+        """Insert the single object into the context dict."""
+        context = {}
+        if self.object:
+            # context['comment_form'] = self.comment_form
+            # context['reply_form'] = self.reply_form
+            slug=self.kwargs['slug']
+            print(slug)
+            blog=Blog.objects.filter(slug=slug).first()
+            # print(blog)
+            # if blog.viewers == None:
+            #         blog.viewers = ""
+            #         blog.save()
+            # user=self.request.user
+            # if user.is_authenticated and user.first_name not in blog.viewers:
+            #     # increment 
+            #     blog.numViews += 1
+            #     blog.save()
+            #     # add username to viewers list
+            #     blog.viewers+=user.first_name
+            #     blog.save()
+            comment=BlogComment.objects.filter(blog=blog,parent=None)
+            replies=BlogComment.objects.filter(blog=blog).exclude(parent=None)
+            replyDict={}
+            for reply in replies:
+                if reply.parent.comment_id not in replyDict.keys():
+                    replyDict[reply.parent.comment_id]=[reply]
+                else: 
+                    replyDict[reply.parent.comment_id].append(reply)
+            # print(replyDict)  
+            context['comment'] = comment
+            context['replies'] = replies
+            context['replyDict'] = replyDict
+            context_object_name = self.get_context_object_name(self.object)
+            if context_object_name:
+                context[context_object_name] = self.object
+            
+        context.update(kwargs)
+        return super().get_context_data(**context)
+    def get_object(self, queryset=None):
+        slug=self.kwargs['slug']
+        obj=Blog.objects.filter(slug=slug).first()
+        if obj.viewers == None:
+                    obj.viewers = ""
+                    obj.save()
+        user=self.request.user
+        if user.is_authenticated and user.first_name not in obj.viewers:
+            # increment 
+            obj.numViews += 1
+            obj.save()
+            # add username to viewers list
+            obj.viewers+=user.first_name
+            obj.save()
+
+        return obj
+    def dispatch(self, *args, **kwargs):
+        slug=self.kwargs['slug']
+        blog=Blog.objects.filter(slug=slug)
+        if not blog.exists():
+            return redirect('home')
+        return super(ContinueReading, self).dispatch(*args, **kwargs)
+        
 
 
 class BlogUpdateView(LoginRequiredMixin, UpdateView):
@@ -51,15 +108,24 @@ class BlogUpdateView(LoginRequiredMixin, UpdateView):
         return context
    
     def get_success_url(self):
-        messages.success(
-            self.request, 'Your post has been updated successfully.')
+        messages.success(self.request, 'Your post has been updated successfully.')
         return reverse_lazy("home")
 
     
-
     def get_queryset(self):
        return  self.model.objects.filter(author=self.request.user)
-       
+
+    def dispatch(self, request, *args, **kwargs):
+        """ Making sure that only authors can update stories """
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            # return redirect(obj)
+            return HttpResponse('Permission Denied')
+        slug=self.kwargs['slug']
+        blog=Blog.objects.filter(slug=slug)
+        if not blog.exists():
+            return redirect('home')
+        return super(UpdateView, self).dispatch(request, *args, **kwargs)
    
 
 class BlogDeleteView(LoginRequiredMixin, DeleteView):
@@ -72,6 +138,14 @@ class BlogDeleteView(LoginRequiredMixin, DeleteView):
 
     def get_queryset(self):
         return self.model.objects.filter(author=self.request.user)
+    def dispatch(self, *args, **kwargs):
+        slug=self.kwargs['slug']
+        blog=Blog.objects.filter(slug=slug,author=self.request.user)
+        if not blog.exists():
+            return redirect('home')
+        
+
+        return super(ContinueReading, self).dispatch(*args, **kwargs)
 
 
 class Login(LoginView):
@@ -167,4 +241,43 @@ class AddPost(CreateView,ProcessFormView):
         blog.slug=slugify(form.cleaned_data['title'])
         #article.save()  # This is redundant, see comments.
         return super(AddPost, self).form_valid(form)
-    
+
+
+
+
+class CommenPostView(LoginRequiredMixin,View):
+
+    '''def get(self,*args,**kwargs):
+      
+        form=CommentForm()
+        print(form)
+        # coupon=CouponForm()
+        # orders=Order.objects.filter(user=self.request.user,ordered=False)
+        context={'form':form}
+        return render(self.request,'blog_post.html',context)'''
+
+    def post(self,*args,**kwargs):
+        comment=self.request.POST.get("comment")
+
+        user=self.request.user
+        post_id=self.request.POST.get("post_id")
+        print(post_id)
+        blog=get_object_or_404(Blog,post_id=post_id)
+        print(blog)
+        # comments = BlogComment.comment.add(comment)
+       
+
+        parentSno=self.request.POST.get("parentSno")
+        print(parentSno)
+        if parentSno=="":
+            comment=BlogComment(comment=comment,blog=blog,user=user)
+            comment.save()
+            messages.success(self.request,'Your comment has been posted successfully!')
+        else:
+            parent=BlogComment.objects.get(comment_id=parentSno)
+            reply=BlogComment(comment=comment,blog=blog,user=user,parent=parent)
+            reply.save()
+            messages.success(self.request,'Your reply has been posted successfully!')
+        
+        return redirect(f'/continue-reading/{blog.slug}')
+        
